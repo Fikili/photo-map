f3fa8f551d122004572518c9ca72f5603b23f190
Ludvik Hobza <ludvik.hobza@tieto.com>
2018-10-05 15:52:02 +0200

    init UI project

---
+++ photo-map-ui/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { AgmCoreModule } from '@agm/core';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AgmCoreModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
